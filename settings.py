1edd6cf7f29c8353b00ef05f1b148fa9b700ddf8
varuagrathore <141573071+varuagrathore@users.noreply.github.com>
2024-06-21 18:40:15 +0530

    Adjusted the game with level setting score and other features

--- settings.py
+++ settings.py
@@ -3,11 +3,13 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0, 0, 0)
-        self.ship_speed = 1.5
+        self.score_color = (255, 255, 255)
+        
+        self.ship_limit =3
 
         # Bullet Settings
         self.bullet_speed = 1.5
-        self.bullet_width = 3
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (253, 253, 253)
         self.bullets_allowed = 3
@@ -17,3 +19,26 @@ class Settings:
         self.fleet_drop_speed =10
         #fleet direction 1 is for right and -1 for left
         self.fleet_direction=1
+
+        #How quickly the game speed up 
+        self.speedup_scale = 1.1
+        self.score_scale =1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 1.5
+        self.bullet_speed = 3
+        self.alien_speed =1.0
+        self.alien_points =  50
+        #fleet direction 1 is for right and -1 for left
+        self.fleet_direction=1
+    
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.aliens_points  =  int(self.alien_points*self.score_scale)
+        print(self.alien_points)
+
